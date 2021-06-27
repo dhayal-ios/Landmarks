f1671e54f3e9d0bf4fbf57ebfbb1657c57bbb205
sd <dhayal.cluzter@gmail.com>
2021-06-27 11:18:22 +0530

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by dhayalprema on 27/06/21.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
